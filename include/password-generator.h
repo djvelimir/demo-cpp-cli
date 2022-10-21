bd29d3668825f737a6b2311ec9d028f1fff912ab
Velimir Đurković <velimir.djurkovic@gmail.com>
2022-10-21 20:55:26 +0200

    Added logic for password generator

--- include/password-generator.h
+++ include/password-generator.h
@@ -5,7 +5,17 @@
 
 class PasswordGenerator : public PasswordGeneratorBase
 {
+    private:
+        int PASSWORD_LENGTH;
+        std::string UPPERCASE_CHARACTERS;
+        std::string LOWERCASE_CHARACTERS;
+        std::string DIGIT_CHARACTERS;
+        std::string SPECIAL_CHARACTERS;
+        std::string UNION_OF_ALLOWED_CHARACTERS;
+
+        char GenerateRandomCharacter(std::string characters);
     public:
+        PasswordGenerator();
         virtual ~PasswordGenerator();
         
         std::string Generate();
